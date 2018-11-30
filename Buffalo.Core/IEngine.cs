0959a698666070bdf585dc911bde538230fe4137
hafei <zhangmengcoding@gmail.com>
2018-12-01 00:17:53 +0800

    add IEngine

---
+++ Buffalo.Core/IEngine.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Buffalo.Core
+{
+    public interface IEngine
+    {
+        void Initialize(IServiceCollection services);
+
+        IServiceProvider ConfigureServices(IServiceCollection services, IConfiguration configuration);
+
+        void ConfigureRequestPipeline(IApplicationBuilder application);
+
+        T Resolve<T>();
+
+        object Resolve(Type type);
+
+        IEnumerable<T> ResolveAll<T>();
+
+        object ResolveUnregistered(Type type);
+    }
+}
