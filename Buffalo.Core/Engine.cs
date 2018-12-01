8ae0a6e9170cac5500decafb91eee26b776c1ff2
hafei <zhangmengcoding@gmail.com>
2018-12-01 22:56:27 +0800

    add staticly singleton methods

---
+++ Buffalo.Core/Engine.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Buffalo.Core
+{
+    [System.Runtime.InteropServices.Guid("3B5A0837-6CE2-4287-BFB1-8F8976C7C1F1")]
+    public class Engine : IEngine
+    {
+        #region Methods
+        public void ConfigureRequestPipeline(IApplicationBuilder application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Initialize(IServiceCollection services)
+        {
+            var provider = services.BuildServiceProvider();
+            var hostingEnvironment = provider.GetRequiredService<IHostingEnvironment>();
+        }
+
+        public T Resolve<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Resolve(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> ResolveAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ResolveUnregistered(Type type)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ Buffalo.Core/Singleton.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Buffalo.Core
+{
+    public class Singleton
+    {
+        static Singleton()
+        {
+            allSingletons = new Dictionary<Type, object>();
+        }
+
+        private static readonly IDictionary<Type, object> allSingletons;
+
+        public static IDictionary<Type, object> AllSingletons
+        {
+            get { return allSingletons; }
+        }
+    }
+
+
+
+    public class Singleton<T> : Singleton
+    {
+        private static T instance;
+
+        public static T Instance
+        {
+            get { return instance; }
+            set
+            {
+                instance = value;
+                AllSingletons[typeof(T)] = value;
+            }
+        }
+    }
+
+    public class SingletonList<T> : Singleton<IList<T>>
+    {
+        static SingletonList()
+        {
+            Singleton<IList<T>>.Instance = new List<T>();
+        }
+
+        public new static IList<T> Instance
+        {
+            get { return Singleton<IList<T>>.Instance; }
+        }
+    }
+
+
+    public class SingleDictionary<TKey, TValue> : Singleton<IDictionary<TKey, TValue>>
+    {
+        static SingleDictionary()
+        {
+            Singleton<IDictionary<TKey, TValue>>.Instance = new Dictionary<TKey, TValue>();
+        }
+
+        public new static IDictionary<TKey, TValue> Instance
+        {
+            get { return Singleton<Dictionary<TKey, TValue>>.Instance; }
+        }
+    }
+}
