a6bdc6ec0aa9c07c80358a97a03f87293e1c35a7
hafei <zhangmengcoding@gmail.com>
2018-12-02 13:47:23 +0800

    add TypeFinder

---
+++ Buffalo.Core/ITypeFinder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Buffalo.Core
+{
+    public interface ITypeFinder
+    {
+        IList<Assembly> GetAllAssemblies();
+
+        IEnumerable<Type> FindClassesOrType(Type assignFrom, bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOrType(Type assignFrom, IEnumerable<Assembly> assemblies, bool
+            onlyConcreteClasses = true);
+
+
+        IEnumerable<Type> FindClassesOrType<T>(bool onlyConcreteClasses = true);
+
+        IEnumerable<Type> FindClassesOrType<T>(IEnumerable<Assembly> assemblies, bool onlyConcreteClasses = true);
+    }
+}
